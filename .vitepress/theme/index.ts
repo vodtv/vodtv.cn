a4bb3d928e93ec186a430aba66fefd8136ddf9af
qq2236639958 <2236639958@qq.com>
2026-09-17 22:48:21 +0800

    modify

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,18 +3,27 @@ import { h } from 'vue'
 import Theme from '@vodtv/theme'
 // 显式引入主题样式
 import '@vodtv/theme/dist/index.css'
+
 import HomeAdvantageCards from './components/HomeAdvantageCards.vue'
 import ScenarioCards from './components/ScenarioCards.vue'
 import ExtensionCards from './components/ExtensionCards.vue'
 import BlogIndex from './components/BlogIndex.vue'
 
 export default {
-  extends: Theme,
+   Layout() {
+    return h((Theme as any).Layout, null, {
+      'aside-ads-before': () => h(ExtensionCards),
+    })
+  },
   enhanceApp(ctx: any) {
     const { app } = ctx
+    
     app.component('HomeAdvantageCards', HomeAdvantageCards)
     app.component('ScenarioCards', ScenarioCards)
     app.component('ExtensionCards', ExtensionCards)
     app.component('BlogIndex', BlogIndex)
+
+    Theme.enhanceApp(ctx)
   },
+                 
 }
